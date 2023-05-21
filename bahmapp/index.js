475ab6f6f65a22b666d9ea97aaeeb131aa6ed185
Honório <hmneton@gmail.com>
2023-05-20 21:41:20 -0300

    adicionado versao front

--- bahmapp/index.js
+++ bahmapp/index.js
@@ -8,7 +8,7 @@ let dadosPonto = null
 const pontosMaps = []
 
 function openViewTable(page, edita, id) {
-  fetch(`pages55/${page}.html`)
+  fetch(`pages56/${page}.html`)
     .then(function (response) {
       return response.text();
     })
@@ -37,7 +37,7 @@ function openView(page, edita, id) {
     dadosPonto = null
     latLgnii = null
   }
-  fetch(`pages55/${page}.html`)
+  fetch(`pages56/${page}.html`)
     .then(function (response) {
       return response.text();
     })
