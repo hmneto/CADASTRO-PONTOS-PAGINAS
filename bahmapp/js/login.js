0f46e5059d2bf85878280fd330f0b94efe34c8f0
Honório <hmneton@gmail.com>
2023-05-21 10:19:51 -0300

    Update login.js

--- bahmapp/js/login.js
+++ bahmapp/js/login.js
@@ -18,8 +18,11 @@ function mySubmitFunction(e) {
   })
     .then((x) => x.json())
     .then((x) => {
+      console.log(x)
       if (x.token) {
         sessionStorage.setItem("loginStore", x.token);
+        sessionStorage.setItem("loginProfile", x.perfilUsuario);
+
         openView("mapa");
       } else {
         document.getElementById('botaoLogin').disabled = false
